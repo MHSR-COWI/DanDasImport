aa4572422f2c9a51ce3cb9c0a770fe125370b5d9
AWNO <awno@cowi.com>
2025-09-22 13:51:35 +0200

    Opdater Python-script til at samle data horisontalt og tilføje funktionalitet til udtræk af ønskede kolonner

--- Python.py
+++ Python.py
@@ -49,19 +49,22 @@ conn_str = (
 conn = pyodbc.connect(conn_str)
 
 # Saml alle dataframes i en liste
-all_dfs = []
+all_dfs_prefixed = []
 
 for name in query_names:
     sql = f"SELECT * FROM [{name}]"
     df = pd.read_sql(sql, conn)
-    # Tilføj evt. kolonne med query-navn til identificering
-    df["KildeQuery"] = name
-    all_dfs.append(df)
+    df = df.dropna(how='all')  # fjern tomme rækker
+    df = df.add_prefix(f"{name}_")  # tilføj præfiks til kolonner for at undgå overlap
+    all_dfs_prefixed.append(df)
 
 conn.close()
 
-# Sammensæt alle DataFrames til én stor DataFrame
-df_samlet = pd.concat(all_dfs, ignore_index=True)
+# Saml data horisontalt (side om side i kolonner)
+df_samlet = pd.concat(all_dfs_prefixed, axis=1)
+
+# Erstat NaN med tomme strenge, så det bliver pænere i Excel
+df_samlet.fillna('', inplace=True)
 
 # Vælg navn og placering på Excel-fil
 excel_path = os.path.join(
@@ -72,7 +75,13 @@ excel_path = os.path.join(
 # Gem til ét ark
 df_samlet.to_excel(excel_path, sheet_name="SamletData", index=False)
 
+# Gem samlet datafil
+df_samlet.to_excel(excel_path, sheet_name="SamletData", index=False)
 print(f"\nAlt data er samlet i arket 'SamletData' i filen:\n{excel_path}")
+
+print(f"\nAlt data er samlet i arket 'SamletData' i filen:\n{excel_path}")
+
+
 
 
 
